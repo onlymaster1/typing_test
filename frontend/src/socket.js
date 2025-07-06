034b3176f0c9e4e9e73ae17d9afb14192cf36821
onlymaster1 <OnlyMaster364@gmail.com>
2025-07-06 14:39:09 +0530

    fixes api link

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io('https://typing-test-2sce.onrender.com', {
   transports: ['websocket'],
   autoConnect: true, // or false if you want to control when it connects
 });
