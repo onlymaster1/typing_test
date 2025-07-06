b8da2a804e2c915aab347308139a0ca2bc7f977e
onlymaster1 <OnlyMaster364@gmail.com>
2025-07-06 14:47:14 +0530

    vite file fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     tailwindcss(),
-  ],
+  base: './', // ✅ This fixes CSS/asset 404 issues on Render
+  plugins: [
+    react(),
+    tailwindcss()
+  ]
 })
